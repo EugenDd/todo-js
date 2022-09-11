67e757027f36f0101117f5ee3922d2fda70c8dfa
NVS <nvskiev@gmail.com>
2022-09-11 11:50:20 +0300

    added todo fom logic

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-const appElement = document.getElementById('app');
+const todoTasks = [];
 
-appElement.innerText = 'hello world';
+const formElement = document.getElementById('todo-form');
+const taskNameElement = document.getElementById('task-name');
+const taskStatusElement = document.getElementById('task-status');
+
+const resetForm = () => {
+    taskNameElement.value = '';
+    taskStatusElement.checked = false;
+}
+
+formElement.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const taskName = taskNameElement.value;
+    const taskStatus = taskStatusElement.checked;
+
+    const currentTask = {
+        taskName,
+        taskStatus
+    }
+
+    todoTasks.push(currentTask);
+
+    console.log('===================================================');
+    console.log('todoTasks', todoTasks);
+    console.log('===================================================');
+
+    resetForm();
+})
+
